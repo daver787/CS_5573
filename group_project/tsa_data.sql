1fec89dd8ec580e56c258a7f64f15cf735babb99
David Espinola <davidespinola@davids-mbp.lan>
2020-04-26 18:47:11 -0500

    Created a parallel script and modified the sql to only have dates in 2005.

--- group_project/tsa_data.sql
+++ group_project/tsa_data.sql
@@ -6,4 +6,5 @@ sum(p.amount) as store_revenue
 from store as s
 inner join staff as st on s.store_id=st.store_id
 inner join payment as p on st.staff_id=p.staff_id
-group by st.store_id,date(payment_date);
+where year(cast(p.payment_date as date))=2005
+group by st.store_id,date(payment_date)
